63a134b8491bdbc8544d0f90d9edb54fe453b326
Alex Thanas <greek313@gmail.com>
2025-11-19 16:50:13 -0600

    Added DELETE, POST and some error handling in POST

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 
 const express = require('express')
 const app = express()
-
 app.use(express.json())
 
 let dateTime = new Date();
+
+
 
 let persons = [
 
@@ -29,26 +30,27 @@ let persons = [
         "number": "39-23-6423122"
     }
 ]
-//
-// app.get("/api/persons", (request,response) => {
-//     response.json(persons)
-// })
+
+
+function getRandomID() {
+    const min = 1;
+    const max = 10000
+return Math.floor(Math.random() * (max-min)) + min;
+}
+
+//GET all
 app.get("/api/persons", (request,response) => {
     response.json(persons);
 })
-
-const getMaxNumPeople = () => {
-    const maxNumPeople = persons.length > 0 ? Math.max(...persons.map(p => Number(p.id))) : 0
-    return String(maxNumPeople);
-}
+//GET info page
 app.get("/info", (request,response) => {
     
-    response.send(`<p>Phonebook has info for  ${getMaxNumPeople()} people</p> <p>Request at : ${dateTime} </p>`)
+    response.send(`<p>Phonebook has info for  ${persons.length} people</p> <p>Request at : ${dateTime} </p>`)
     
 })
-
+//GET specific person
 app.get("/api/persons/:id", (request,response) => {
-    const id = Number(request.params.id);
+    const id = request.params.id;
     const person = persons.find(person => person.id === id.toString());
     
     if(person) {
@@ -57,7 +59,52 @@ app.get("/api/persons/:id", (request,response) => {
         response.status(404).end();
     }
 }) 
+//DELETE
+app.delete('/api/persons/:id', (request,response) => {
+    const id = request.params.id;
+    persons = persons.filter(person => person.id !== id);
 
+    response.status(204).end();
+})
+//POST
+app.post('/api/persons', (request,response) => {
+    const body = request.body
+    const personFound = persons.find((e) => e.name == body.name)
+
+    if(personFound){
+        return response.status(400).json({
+            error: 'Name must be unique'
+        })
+    }
+
+    if(!body.name) {
+        return response.status(400).json({
+            error: 'name is missing'  
+        })
+    }
+    if(!body.number) {
+        return response.status(400).json({
+            error: 'number is missing'
+        })
+    }
+    const person = {
+        id: String(getRandomID()),
+        name: body.name,
+        number: body.number
+    }
+    persons = persons.concat(person)
+
+    response.json(person)
+
+})
+
+
+
+
+
+
+
+// driver setting port
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`);
