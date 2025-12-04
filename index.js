3306c87aa03dc7cfbcee7e6de9aab810b9d70f24
Alex Thanas <greek313@gmail.com>
2025-12-04 14:29:39 -0600

    Eslint installed and adhered

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
- require('dotenv').config();
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
@@ -13,33 +13,33 @@ app.use(express.static('dist'))
 // app.use(morgan('tiny'))
 //Morgan token looking for POST method and returning a JSON'd body
 morgan.token('post', function (tokens,req,res) {
-    if(tokens.method(req,res) === 'POST'){
-        return JSON.stringify(req.body)
-    }
+  if(tokens.method(req,res) === 'POST'){
+    return JSON.stringify(req.body)
+  }
 })
 
 // Morgan middleware using custom configuration
 app.use(morgan(function (tokens,req,res) {
-    return[
-        tokens.method(req, res),
-        tokens.url(req, res),
-        tokens.status(req, res),
-        tokens.res(req, res, 'content-length'), '-',
-        tokens['response-time'](req, res), 'ms',
-        // custom token being called
-        tokens.post(tokens,req,res)
-    ].join(' ')
+  return[
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+    // custom token being called
+    tokens.post(tokens,req,res)
+  ].join(' ')
 }))
 
-app.use(cors());
+app.use(cors())
 
 
 
 //GET all database
-app.get("/api/persons", (request,response) => {
-    Person.find({}).then(persons => {
-        response.json(persons);
-    })
+app.get('/api/persons', (request,response) => {
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
 
 //GET all
@@ -47,102 +47,102 @@ app.get("/api/persons", (request,response) => {
 //     response.json(persons);
 // })
 //GET info page
-app.get("/info", async (request,response) => {
-    const count = await Person.countDocuments({});
-    
-    let dateTime = new Date();
+app.get('/info', async (request,response) => {
+  const count = await Person.countDocuments({})
 
-    response.send(`<p>Phonebook has info for  ${count} people</p> <p>Request at : ${dateTime} </p>`)
-    
+  let dateTime = new Date()
+
+  response.send(`<p>Phonebook has info for  ${count} people</p> <p>Request at : ${dateTime} </p>`)
+
 })
 //GET specific person
-app.get("/api/persons/:id", (request,response,next) => {
-    // const id = request.params.id;
-    // const person = persons.find(person => person.id === id.toString());
-        Person.findById(request.params.id).then(person => {
-            response.json(person)
-        })
-        .catch(error => next(error))
-   
-}) 
+app.get('/api/persons/:id', (request,response,next) => {
+  // const id = request.params.id;
+  // const person = persons.find(person => person.id === id.toString());
+  Person.findById(request.params.id).then(person => {
+    response.json(person)
+  })
+    .catch(error => next(error))
+
+})
 //DELETE
 app.delete('/api/persons/:id', (request,response,next) => {
-    Person.findByIdAndDelete(request.params.id)
-    .then(result => {
-        response.status(204).end();      
+  Person.findByIdAndDelete(request.params.id)
+    .then(() => {
+      response.status(204).end()
     })
     .catch(error => next(error))
 })
 //POST
 app.post('/api/persons', (request,response,next) => {
-    const body = request.body
+  const body = request.body
 
-    if(!body.name) {
-        return response.status(400).json({
-            error: 'name is missing'  
-        })
-    }
-    if(!body.number) {
-        return response.status(400).json({
-            error: 'number is missing'
-        })
-    }
-    const person =  new Person({
-        name: body.name,
-        number: body.number,
+  if(!body.name) {
+    return response.status(400).json({
+      error: 'name is missing'
     })
+  }
+  if(!body.number) {
+    return response.status(400).json({
+      error: 'number is missing'
+    })
+  }
+  const person =  new Person({
+    name: body.name,
+    number: body.number,
+  })
 
-    person.save().then(savedPerson => {
-        response.json(savedPerson);
-    })
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
     .catch(error => next(error))
 
 })
 
 //PUT
 app.put('/api/persons/:id', (request,response,next) => {
-    const {name, number} = request.body
+  const { name, number } = request.body
 
-    Person.findById(request.params.id)
+  Person.findById(request.params.id)
     .then(person => {
-        if(!person){
-           return response.status(404).end();
-        }
+      if(!person){
+        return response.status(404).end()
+      }
 
-        person.name = name
-        person.number = number
+      person.name = name
+      person.number = number
 
-        return person.save().then(updatedPerson => {
-            response.json(updatedPerson)
-        })
+      return person.save().then(updatedPerson => {
+        response.json(updatedPerson)
+      })
     })
     .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'Unkown Endpoint' })
+  response.status(404).send({ error: 'Unkown Endpoint' })
 }
 
-app.use(unknownEndpoint);
+app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
+  console.error(error.message)
 
-    if (error.name === 'CastError') {
-        return response.status(400).send({ error: 'malformatted id' })
-    }else if (error.name === 'ValidationError') {
-        return response.status(400).json({error: error.message})
-    } 
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
 
-    next(error)
+  next(error)
 }
 
-app.use(errorHandler);
+app.use(errorHandler)
 
 
 
 // driver setting port
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-    console.log(`Server running on ${PORT}`);
+  console.log(`Server running on ${PORT}`)
 })
